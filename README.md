2d6bc29fcac104de4c81ad372a3bf43934d9d959
valdo99 <edvaldo99@icloud.com>
2022-06-01 23:35:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-nextjs-m7pkb6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-nextjs-m7pkb6)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dracula',
+  },
+};
