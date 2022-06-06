e987cf702f1d3960aa1fe96a254e820cc8c26a5b
valdo99 <edvaldo99@icloud.com>
2022-06-06 12:17:59 +0200

    hot fix

--- UI/navbar/navbar.js
+++ UI/navbar/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar({}) {
   // const { session } = data;
 
   return (
-    <div class="navbar bg-base-100 px-10">
+    <div class="navbar bg-base-100 xl:px-32 md:px-10 xs:pr-8 py-6">
       <div class="navbar-start">
         <div class="dropdown">
           <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -91,12 +91,17 @@ export default function Navbar({}) {
           </li>
         </ul>
       </div>
-      <div class="navbar-end">
+      <div class="navbar-end ">
         {session?.session?.user?.name ? (
           <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
-                <img src="https://api.lorem.space/image/face?hash=33791" />
+            <label
+              tabindex="0"
+              class="btn btn-ghost btn-circle avatar placeholder"
+            >
+              <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <span class="text-xl">
+                  {session?.session?.user?.name.slice(0, 1)}
+                </span>
               </div>
             </label>
             <ul
@@ -107,10 +112,7 @@ export default function Navbar({}) {
                 <p>{session?.session?.user?.name}</p>
               </li>
               <li>
-                <a class="justify-between">
-                  Profile
-                  <span class="badge">New</span>
-                </a>
+                <a class="justify-between">Profile</a>
               </li>
               <li>
                 <a>Settings</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,23 @@ module.exports = {
   daisyui: {
     themes: ["dracula"],
   },
+  theme: {
+    screens: {
+      "2xl": { min: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { min: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { min: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { min: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { min: "639px" },
+      xs: { min: "200px" },
+      // => @media (max-width: 639px) { ... }
+    },
+  },
 };
